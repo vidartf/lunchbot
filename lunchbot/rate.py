cd5806325e4d5dd135ccd69588ae427939e43b47
Vidar Tonaas Fauske <vidartf@gmail.com>
2019-03-07 23:09:48 +0100

    Utils to rate to menus against each other
    Can be used to sort historical menus in order.

---
+++ lunchbot/rate.py
@@ -0,0 +1,98 @@
+
+from datetime import date
+from itertools import combinations
+from functools import partial
+
+from .use_historical import iter_menus
+
+try:
+    input = raw_input
+except NameError: #Python 3
+    pass
+
+
+def all_menu_pairs():
+    return combinations(iter_menus(), 2)
+
+
+import sqlite3
+_db = None
+
+def _connect():
+    global _db
+    _db = sqlite3.connect('comparisons.sqlite')
+    _db.execute('''
+        CREATE TABLE IF NOT EXISTS
+        comparison (
+            a INTEGER NOT NULL,
+            b INTEGER NOT NULL,
+            cmp INTEGER,
+            PRIMARY KEY(a, b)
+        ) WITHOUT ROWID;
+    ''')
+
+epoch = date(2010, 1, 1)
+
+def _dbkey(e):
+    d = e.date - epoch
+    return 10 * d.days + (1 if e.floor == 'third' else 0)
+
+
+def _previous(a, b):
+    rows = _db.execute('''
+        SELECT cmp FROM (
+            SELECT * FROM comparison
+            WHERE a = ? AND b = ?
+        )
+        LIMIT 1
+    ''', (_dbkey(a), _dbkey(b)))
+    for r in rows:
+        return r[0]
+    return None
+
+def prompt_comparison(a, b):
+    prev = _previous(a, b)
+    if prev is not None:
+        return prev
+    print('Choose the best menu:')
+    print('=' * 60)
+    print(a.menu)
+    print('-' * 60)
+    print(b.menu)
+    print('-' * 60)
+    res = 0
+    while True:
+        reply = input('[a/b]:')
+        if reply == 'a':
+            res = -1
+            break
+        elif reply == 'b':
+            res = 1
+            break
+    _db.execute('''
+        INSERT INTO comparison (a, b, cmp)
+        VALUES (?, ?, ?)
+    ''', (_dbkey(a), _dbkey(b), res))
+    return res
+
+
+try:
+    from functools import cmp_to_key
+    _sort_menus = partial(sorted, key=cmp_to_key(prompt_comparison))
+except ImportError:
+    _sort_menus = partial(sorted, cmp=prompt_comparison)
+
+
+def prompt_sort(entries):
+    return _sort_menus(entries)
+
+
+if __name__ == '__main__':
+    from .main import logger
+    logger.setLevel('ERROR')
+    _connect()
+    try:
+        print(prompt_sort(iter_menus()))
+    finally:
+        _db.commit()
+        _db.close()
